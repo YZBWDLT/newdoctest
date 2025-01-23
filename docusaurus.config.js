4e23e860036179d635862940e640ac84222a0d0e
yzbwdlt <yzbwdlt@163.com>
2025-01-23 10:18:06 +0800

    同上

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,23 +46,6 @@ const config = {
                     editUrl: 'https://github.com/PumpkinJui/groupdocs/tree/main/',
                 },
                 blog: false,
-                /**
-                {
-                    showReadingTime: true,
-                    feedOptions: {
-                        type: ['rss', 'atom'],
-                        xslt: true,
-                    },
-                    // Please change this to your repo.
-                    // Remove this to remove the 'edit this page' links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                    // Useful options to enforce blogging best practices
-                    onInlineTags: 'warn',
-                    onInlineAuthors: 'warn',
-                    onUntruncatedBlogPosts: 'warn',
-                }
-                 */
                 theme: {
                     customCss: './src/css/custom.css',
                 },
@@ -121,10 +104,6 @@ const config = {
                 {
                     title: 'More',
                     items: [
-                        {
-                            label: 'Blog',
-                            to: '/blog',
-                        },
                         {
                             label: 'GitHub',
                             href: 'https://github.com/PumpkinJui/groupdocs',
