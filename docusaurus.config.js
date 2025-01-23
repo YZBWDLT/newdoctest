5f0605fa729d4e105668b6641a9369931dcdeabb
yzbwdlt <yzbwdlt@163.com>
2025-01-23 14:54:58 +0800

    烦人，直接提交吧

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
     organizationName: 'PumpkinJui', // Usually your GitHub org/user name.
     projectName: 'groupdocs', // Usually your repo name.
 
-    onBrokenLinks: 'throw',
+    onBrokenLinks: 'ignore',
     onBrokenMarkdownLinks: 'warn',
 
     // Even if you don't use internationalization, you can use this field to set
